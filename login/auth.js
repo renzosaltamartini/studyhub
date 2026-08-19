6a1d4d87005ba593ffbb1cd0c74c2508e1d70e75
renzosaltamartini <renzosaltamartini2008@gmail.com>
2026-08-18 22:21:44 -0300

    asd

--- login/auth.js
+++ login/auth.js
@@ -4,7 +4,7 @@ import {
     signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword,
     updateProfile, sendPasswordResetEmail, onAuthStateChanged, signOut
 } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
-import { firebaseConfig } from "./firebase-config.js";
+import { firebaseConfig } from "/login/firebase-config.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
