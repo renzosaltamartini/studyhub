d3214219b0c3308563ab60592d7503d876719c84
renzosaltamartini <renzosaltamartini2008@gmail.com>
2026-08-18 22:37:20 -0300

    asd

--- login/firebase-config.js
+++ login/firebase-config.js
@@ -1,9 +1,11 @@
 // Reemplaza estos valores por los de Firebase Console > Configuración del proyecto > Tus apps.
 export const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_PROYECTO.firebaseapp.com",
-    projectId: "TU_PROYECTO",
-    storageBucket: "TU_PROYECTO.firebasestorage.app",
-    messagingSenderId: "TU_MESSAGING_SENDER_ID",
-    appId: "TU_APP_ID"
+  apiKey: "AIzaSyAXGKWZA8kWaN2NWCK2A9jTlGSq-B2bqWA",
+  authDomain: "laranchada-6abd3.firebaseapp.com",
+  databaseURL: "https://laranchada-6abd3-default-rtdb.firebaseio.com",
+  projectId: "laranchada-6abd3",
+  storageBucket: "laranchada-6abd3.firebasestorage.app",
+  messagingSenderId: "326750839699",
+  appId: "1:326750839699:web:7d5f46d67b20ad2599fe66",
+  measurementId: "G-0QP3VVQZ0Z"
 };
