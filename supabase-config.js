e238cfaa61fd8a91b5bf122a46e6ba9a0a5ad3bc
unknown <renzosaltamartini2008@gmail.com>
2026-08-23 20:33:42 -0300

    asd

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 // Datos públicos de Supabase: Project Settings > Data API.
 // La service_role NUNCA debe colocarse en este archivo.
 export const supabaseConfig = {
-  url: "https://veakfkfdkojbohcpgrxt.supabase.co/rest/v1/",
+  url: "https://veakfkfdkojbohcpgrxt.supabase.co",
   anonKey: "sb_publishable_xRWH_NBI5gxcJXDyfp22DA_toqSbv0y",
   bucket: "studyhub-files"
 };
